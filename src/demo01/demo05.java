97b63816d7d2fc69e0d20479635aba9886b290a7
年迈的老头子 <oncwnuOKvA_bkeOceXDgNqaioUgI@git.weixin.qq.com>
2020-05-30 19:10:15 +0800

    test

--- src/demo01/demo05.java
+++ src/demo01/demo05.java
@@ -5,7 +5,7 @@ package demo01;
  */
 public class demo05 {
     public static void main(String[] args){
-        //使用Math类生成随机数，范围是【0.0，1.0）
+        //使用Math类生成随机数，范围是【0.0，1.0）；
         double voe = Math.random()*90;
         System.out.println(voe);
     }
